6695f3d60f704eaa80a893a9a86255e3fa412a6c
Lucas Orsini <lucasorsini@gmail.com>
2024-07-23 23:35:16 -0300

    interface de usuario con materialize-se hizo el login y el menu ppal

---
+++ serviTech/wwwroot/js/menu.js
@@ -0,0 +1,12 @@
+/* Inicializacion Menu lateral */
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+
+/* Inicializacion modal */
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
