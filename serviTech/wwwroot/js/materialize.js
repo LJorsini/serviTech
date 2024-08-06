cb768dc13337a4b7a58a62e810c28d2972af8906
Lucas Orsini <lucasorsini@gmail.com>
2024-08-06 18:10:22 -0300

    respaldo

--- serviTech/wwwroot/js/materialize.js
+++ serviTech/wwwroot/js/materialize.js
@@ -17,4 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Collapsible.init(elems);
   });
 
+  document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
+
  
